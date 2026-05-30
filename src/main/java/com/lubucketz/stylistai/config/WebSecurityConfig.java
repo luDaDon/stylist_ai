2a3906fd5894f03cebdbfa6c69bbc14f83697395
MoneyMakinManiac <luthbell4@gmail.com>
2026-05-30 16:57:48 -0500

    update security config to secure endpoints

--- src/main/java/com/lubucketz/stylistai/config/WebSecurityConfig.java
+++ src/main/java/com/lubucketz/stylistai/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.lubucketz.stylistai.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -13,7 +14,14 @@ public class WebSecurityConfig {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
+                        .requestMatchers(
+                                "/v1/users",
+                                "/v1/users/login"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(Customizer.withDefaults())
                 )
                 .build();
     }
