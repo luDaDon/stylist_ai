762a44a9f08a917d8ed020869152669080e346f4
MoneyMakinManiac <luthbell4@gmail.com>
2026-05-30 13:23:18 -0500

    initial

---
+++ src/test/java/com/lubucketz/stylistai/StylistAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lubucketz.stylistai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StylistAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
