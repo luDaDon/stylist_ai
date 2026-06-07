01142f3c348f3a7173f7497ed2b74cc1232a12c6
luDaDon <luthbell4@gmail.com>
2026-06-06 19:18:58 -0500

    add request and endpoint to create a clothing item

---
+++ src/main/java/com/lubucketz/stylistai/clothingitem/ClothingItemController.java
@@ -0,0 +1,32 @@
+package com.lubucketz.stylistai.clothingitem;
+
+import com.lubucketz.stylistai.domain.ClothingItem;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/clothing-items")
+public class ClothingItemController {
+
+    private final ClothingItemSupplier supplier;
+
+    public ClothingItemController(ClothingItemSupplier supplier){
+        this.supplier = supplier;
+    }
+
+    @PostMapping
+    public ResponseEntity<ClothingItem> create(
+            @RequestBody CreateClothingItemRequest request
+    ) {
+        ClothingItem clothingItem = supplier.create(
+                request
+        );
+
+        return ResponseEntity.ok(clothingItem);
+    }
+}
