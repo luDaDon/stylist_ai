fdd149bfcd187f6fbe0635390eb876a3318c7cad
luDaDon <luthbell4@gmail.com>
2026-06-13 15:31:04 -0500

    remove unused imports

--- src/main/java/com/lubucketz/stylistai/clothingitem/ClothingItemController.java
+++ src/main/java/com/lubucketz/stylistai/clothingitem/ClothingItemController.java
@@ -2,8 +2,6 @@ package com.lubucketz.stylistai.clothingitem;
 
 import com.lubucketz.stylistai.domain.ClothingItem;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
