771861e6b039e637167a77c735d24bfb3c24de3c
luDaDon <luthbell4@gmail.com>
2026-06-13 16:52:20 -0500

    finish clothing items crud

--- src/main/java/com/lubucketz/stylistai/clothingitem/ClothingItemSupplier.java
+++ src/main/java/com/lubucketz/stylistai/clothingitem/ClothingItemSupplier.java
@@ -2,6 +2,16 @@ package com.lubucketz.stylistai.clothingitem;
 
 import com.lubucketz.stylistai.domain.ClothingItem;
 
+import java.util.Collection;
+import java.util.UUID;
+
 public interface ClothingItemSupplier {
     ClothingItem create( CreateClothingItemRequest request );
+    Collection<ClothingItem> getByUser();
+    ClothingItem getById(UUID id);
+    void delete(UUID id);
+    ClothingItem update(
+            UUID id,
+            UpdateClothingItemRequest request
+    );
 }
